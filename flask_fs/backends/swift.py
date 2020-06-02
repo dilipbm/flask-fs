c665c0a3c26e05b6bee026ac1fe87573f7fda9be
dilip <dilip@hotmail.fr>
2020-06-02 20:42:59 +0200

    add compatibility swift v3

--- flask_fs/backends/swift.py
+++ flask_fs/backends/swift.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 
 
 class SwiftBackend(BaseBackend):
-    '''
+    """
     An OpenStack Swift backend
 
     Expect the following settings:
@@ -23,15 +23,25 @@ class SwiftBackend(BaseBackend):
     - `authurl`: The Swift Auth URL
     - `user`: The Swift user in
     - `key`: The user API Key
-    '''
+    """
+
     def __init__(self, name, config):
         super(SwiftBackend, self).__init__(name, config)
 
+        # self.conn = swiftclient.Connection(
+        #     user=config.user,
+        #     key=config.key,
+        #     authurl=config.authurl
+        # )
+
         self.conn = swiftclient.Connection(
+            authurl=config.authurl,
             user=config.user,
             key=config.key,
-            authurl=config.authurl
+            auth_version=config.authversion,
+            os_options=config.osoptions,
         )
+
         self.conn.put_container(self.name)
 
     def exists(self, filename):
@@ -42,12 +52,12 @@ class SwiftBackend(BaseBackend):
             return False
 
     @contextmanager
-    def open(self, filename, mode='r', encoding='utf8'):
-        if 'r' in mode:
+    def open(self, filename, mode="r", encoding="utf8"):
+        if "r" in mode:
             obj = self.read(filename)
-            yield io.BytesIO(obj) if 'b' in mode else io.StringIO(obj.decode(encoding))
+            yield io.BytesIO(obj) if "b" in mode else io.StringIO(obj.decode(encoding))
         else:  # mode == 'w'
-            f = io.BytesIO() if 'b' in mode else io.StringIO()
+            f = io.BytesIO() if "b" in mode else io.StringIO()
             yield f
             self.write(filename, f.getvalue())
 
@@ -64,22 +74,22 @@ class SwiftBackend(BaseBackend):
         else:
             headers, items = self.conn.get_container(self.name, path=filename)
             for i in items:
-                self.conn.delete_object(self.name, i['name'])
+                self.conn.delete_object(self.name, i["name"])
 
     def copy(self, filename, target):
-        dest = '/'.join((self.name, target))
+        dest = "/".join((self.name, target))
         self.conn.copy_object(self.name, filename, destination=dest)
 
     def list_files(self):
         headers, items = self.conn.get_container(self.name)
         for i in items:
-            yield i['name']
+            yield i["name"]
 
     def get_metadata(self, filename):
         data = self.conn.head_object(self.name, filename)
         return {
-            'checksum': 'md5:{0}'.format(data['etag']),
-            'size': int(data['content-length']),
-            'mime': data['content-type'],
-            'modified': parser.parse(data['last-modified']),
+            "checksum": "md5:{0}".format(data["etag"]),
+            "size": int(data["content-length"]),
+            "mime": data["content-type"],
+            "modified": parser.parse(data["last-modified"]),
         }
